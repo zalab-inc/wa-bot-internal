865e2991d1f5107454e51ecb0e58243c0deba494
zalab-inc <zalabinc@gmail.com>
2025-02-16 03:21:45 +0700

    add task

--- src/index.ts
+++ src/index.ts
@@ -284,7 +284,7 @@ class WhatsAppService {
 
 		// Example: Send daily reminder at 9 AM Jakarta time
 		cron.schedule(
-			"0 */3 * * *",
+			"0 */2 * * *",
 			async () => {
 				try {
 					const groupId = "120363321235164609@g.us"; // fiverr
